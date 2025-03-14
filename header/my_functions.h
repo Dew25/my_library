4aeb68d89803d49b1185edd66e1d68cd0cf53724
Juri Melnikov <melnikov@F245-LAB.net>
2025-03-14 11:31:39 +0200

    Добавили утилиту для вычисления сдвигов и функцию вычисления возраста по дате рождения

--- header/my_functions.h
+++ header/my_functions.h
@@ -1,6 +1,9 @@
 #ifndef MY_FUNCTIONS_H
 #define MY_FUNCTIONS_H
 
-int readln(char s[], int maxlen);
 
+int readln(char s[], int maxlen);
+void printBinary(unsigned char port);
+void run_shift_arifmetic();
+int calculate_age(int birth_day, int birth_month, int birth_year);
 #endif
